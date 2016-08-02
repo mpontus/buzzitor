b62687e0018c7f924a0d184dfbb27c07450ad7fd
Michael Pontus <m.pontus@gmail.com>
2016-08-02 13:53:48 +0300

    Restore "require_tree ." to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require_tree .
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('serviceworker.js').then(initialiseState);

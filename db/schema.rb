ca6fe911ef92d38be4306ac9d35f01041b9d16e4
Michael Pontus <m.pontus@gmail.com>
2016-08-12 16:33:10 +0300

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801164953) do
+ActiveRecord::Schema.define(version: 20160801162914) do
 
   create_table "monitorings", force: :cascade do |t|
-    t.string   "address"
+    t.string   "url"
     t.string   "endpoint"
     t.binary   "content"
     t.string   "error"
+    t.datetime "fetched_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "fetched_at"
   end
 
 end

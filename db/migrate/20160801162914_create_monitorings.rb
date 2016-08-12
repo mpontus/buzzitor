d6028c9b38cbe5370925f362339e49a197d406d2
Michael Pontus <m.pontus@gmail.com>
2016-08-12 16:30:51 +0300

    Merge fetched_at in initial migration

--- db/migrate/20160801162914_create_monitorings.rb
+++ db/migrate/20160801162914_create_monitorings.rb
@@ -1,10 +1,11 @@
 class CreateMonitorings < ActiveRecord::Migration[5.0]
   def change
     create_table :monitorings do |t|
-      t.string :address
+      t.string :url
       t.string :endpoint
       t.binary :content
       t.string :error
+      t.datetime :fetched_at
 
       t.timestamps
     end
--- db/migrate/20160801164953_add_fetched_at_to_monitoring.rb
+++
@@ -1,5 +0,0 @@
-class AddFetchedAtToMonitoring < ActiveRecord::Migration[5.0]
-  def change
-    add_column :monitorings, :fetched_at, :datetime
-  end
-end

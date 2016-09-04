cdd29325cc15ec7fa1a61e3b02e1f6a4b10b70c3
Michael Pontus <m.pontus@gmail.com>
2016-09-04 07:27:11 +0300

    Revert "Switch rpush to ActiveRecord"
    This reverts commit 810c78ace339639f47341ad9896fb8dd816bc309.

--- app/models/monitoring/subscriber.rb
+++ app/models/monitoring/subscriber.rb
@@ -13,7 +13,7 @@ module Monitoring
     def notify(message="Website updated!")
       registration_id = File.basename(URI(endpoint).path)
       n = Rpush::Gcm::Notification.new
-      n.app = Rpush::Gcm::App.find_by_name("android_app")
+      n.app = Rpush::Gcm::App.where(name: "android_app").first #find_by_name("android_app")
       n.registration_ids = [ registration_id ]
       n.priority = 'high'
       n.content_available = true

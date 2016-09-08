c54364cb4f08c2b49a78cb4c10eac38c6e61ceda
Michael Pontus <m.pontus@gmail.com>
2016-09-08 15:11:37 +0300

    Disable Rufus Scheduler in test environment

--- config/initializers/rufus-scheduler.rb
+++ config/initializers/rufus-scheduler.rb
@@ -1,13 +1,15 @@
 require 'rufus-scheduler'
 
-scheduler = Rufus::Scheduler::singleton
+if Rails.env != 'test'
+  scheduler = Rufus::Scheduler::singleton
 
-scheduler.every '5s' do
-  interval = 10
-  Monitoring::Context
-    .where('fetched_at < :before',
-           { before: interval.seconds.ago })
-    .each do |context|
-    FetchJob.perform_later(context)
+  scheduler.every '5s' do
+    interval = 10
+    Monitoring::Context
+      .where('fetched_at < :before',
+             { before: interval.seconds.ago })
+      .each do |context|
+      FetchJob.perform_later(context)
+    end
   end
 end

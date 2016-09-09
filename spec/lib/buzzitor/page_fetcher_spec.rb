aac0d6d609bc4728b98101d8da9ca843737d59e0
Michael Pontus <m.pontus@gmail.com>
2016-09-10 02:23:01 +0300

    Refactor page_fetcher_spec

--- spec/lib/buzzitor/page_fetcher_spec.rb
+++ spec/lib/buzzitor/page_fetcher_spec.rb
@@ -11,28 +11,26 @@ RSpec.describe Buzzitor::PageFetcher do
   end
 
   subject do
-    proc do
-      content = nil
-      Buzzitor::PageFetcher::fetch(@test_app_url) do |driver|
-        content = driver.html
-      end
-      content
+    content = nil
+    Buzzitor::PageFetcher::fetch(@test_app_url) do |driver|
+      content = driver.html
     end
+    content
   end
 
   it "fetches remote content" do
     @app.content = 'Foobar';
-    expect(subject.call).to include("Foobar")
+    expect(subject).to include("Foobar")
   end
 
   it "evaluates javascript on fetched page" do
     @app.content = '<script>document.write("foo"+"bar");</script>';
-    expect(subject.call).to include("foobar")
+    expect(subject).to include("foobar")
   end
 
   it "doesn't stumble on javascript errors" do
-    @app.content = '<script>throw "Error";</script> FooBar';
-    expect(subject.call).to include("FooBar")
+    @app.content = '<script>document.write("Foo"+"Bar"); throw "Error";</script>';
+    expect(subject).to include("FooBar")
   end
 
 end

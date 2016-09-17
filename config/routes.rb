38be7dabcc6322d6be9f5e142cb736451a6af96b
Michael Pontus <m.pontus@gmail.com>
2016-09-17 17:19:57 +0300

    Map context resource to root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'monitoring/contexts#new'
   get '/serviceworker', to: 'core#serviceworker'
   get '/manifest',      to: 'core#manifest'
-  namespace :monitoring do
-    resources :contexts  do
+  namespace :monitoring, path: nil, shallow: true do
+    resources :contexts, path: '/'  do
       resources :subscribers, only: [:create]
       # member do
       # end
